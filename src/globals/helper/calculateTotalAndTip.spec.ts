335cc1e61398391765a9b0624a5e0030aaab4929
kephalosk <watchfromtheshore@gmail.com>
2025-04-23 03:36:38 +0200

    add helper calculateTotalAndTip

---
+++ src/globals/helper/calculateTotalAndTip.spec.ts
@@ -0,0 +1,51 @@
+import calculateTotalAndTip from "@/globals/helper/calculateTotalAndTip.ts";
+import {
+  CALCULATE_TOTAL_AND_TIP_ERROR_MESSAGE,
+  EMPTY_PRICE_DECIMAL_STRING,
+} from "@/globals/constants/constants.ts";
+
+jest.spyOn(console, "error").mockImplementation((): void | null => null);
+
+describe("calculateTotalAndTip", (): void => {
+  it("calculates tipTotal and totalSum for valid Input", (): void => {
+    const billAmount: number = 1000;
+    const tipAmount: number = 10;
+    const peopleCount: number = 10;
+
+    const expectedTipTotal: string = "10.00";
+    const expectedTotalSum: string = "110.00";
+
+    const { tipTotal, totalSum } = calculateTotalAndTip(
+      billAmount,
+      tipAmount,
+      peopleCount,
+    );
+
+    expect(tipTotal).toEqual(expectedTipTotal);
+    expect(totalSum).toEqual(expectedTotalSum);
+  });
+
+  it.each([
+    [0, 1, 1],
+    [1, 0, 1],
+    [1, 1, 0],
+    [-1, 1, 1],
+    [1, -1, 1],
+    [1, 1, -1],
+  ])(
+    "returns empty price strings if billAmount = %s, tipAmount = %s, peopleCount = %s",
+    (billAmount: number, tipAmount: number, peopleCount: number): void => {
+      const { tipTotal, totalSum } = calculateTotalAndTip(
+        billAmount,
+        tipAmount,
+        peopleCount,
+      );
+
+      expect(tipTotal).toEqual(EMPTY_PRICE_DECIMAL_STRING);
+      expect(totalSum).toEqual(EMPTY_PRICE_DECIMAL_STRING);
+      expect(console.error).toHaveBeenCalledWith(
+        CALCULATE_TOTAL_AND_TIP_ERROR_MESSAGE,
+      );
+    },
+  );
+});
---
+++ src/globals/helper/calculateTotalAndTip.ts
@@ -0,0 +1,30 @@
+import {
+  CALCULATE_TOTAL_AND_TIP_ERROR_MESSAGE,
+  EMPTY_PRICE_DECIMAL_STRING,
+} from "@/globals/constants/constants.ts";
+
+const calculateTotalAndTip = (
+  billAmount: number,
+  tipAmount: number,
+  peopleCount: number,
+): { tipTotal: string; totalSum: string } => {
+  if (billAmount <= 0 || tipAmount <= 0 || peopleCount <= 0) {
+    console.error(CALCULATE_TOTAL_AND_TIP_ERROR_MESSAGE);
+    return {
+      tipTotal: EMPTY_PRICE_DECIMAL_STRING,
+      totalSum: EMPTY_PRICE_DECIMAL_STRING,
+    };
+  }
+
+  const calculatedTip: number = billAmount * (tipAmount / 100);
+  const calculatedTipPerPerson: string = (calculatedTip / peopleCount).toFixed(
+    2,
+  );
+
+  const total: number = billAmount + calculatedTip;
+  const totalPerPerson: string = (total / peopleCount).toFixed(2);
+
+  return { tipTotal: calculatedTipPerPerson, totalSum: totalPerPerson };
+};
+
+export default calculateTotalAndTip;
