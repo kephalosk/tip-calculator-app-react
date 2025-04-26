248e301a27fa25436555b500aa100c40763f70b5
kephalosk <watchfromtheshore@gmail.com>
2025-04-26 18:23:37 +0200

    refactor ResultContainer component

--- src/components/container/ResultContainer/ResultContainer.spec.tsx
+++ src/components/container/ResultContainer/ResultContainer.spec.tsx
@@ -1,18 +1,132 @@
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "@/redux/store.ts";
-import App from "@/App.tsx";
+import PriceContainer from "@/components/container/PriceContainer/PriceContainer.tsx";
+import Button from "@/components/atoms/Button/Button.tsx";
+import { ReactElement } from "react";
+import { useResultCalculations } from "@/hooks/useResultCalculations.ts";
+import { render, screen } from "@testing-library/react";
+import ResultContainer from "@/components/container/ResultContainer/ResultContainer.tsx";
+import { useResetFields } from "@/hooks/redux/useResetFields.ts";
+import {
+  BUTTON_TEXT_RESET,
+  PRICE_SECTION_LABEL_TEXT_AMOUNT,
+  PRICE_SECTION_LABEL_TEXT_TOTAL,
+} from "@/globals/constants/constants.ts";
 
-describe("App", (): void => {
-  it("renders App", (): void => {
-    const { container } = render(
-      <Provider store={store}>
-        <App />
-      </Provider>,
-    );
+const priceContainerTestId: string = "price-container";
+jest.mock(
+  "@/components/container/PriceContainer/PriceContainer.tsx",
+  (): jest.Mock =>
+    jest.fn((): ReactElement => {
+      return <div data-testid={priceContainerTestId}></div>;
+    }),
+);
 
-    const element: HTMLElement | null = container.querySelector(".app");
+const buttonTestId: string = "button";
+jest.mock(
+  "@/components/atoms/Button/Button.tsx",
+  (): jest.Mock =>
+    jest.fn((): ReactElement => {
+      return <div data-testid={buttonTestId}></div>;
+    }),
+);
+
+jest.mock(
+  "@/hooks/useResultCalculations.ts",
+  (): {
+    __esModule: boolean;
+    useResultCalculations: jest.Mock;
+  } => ({
+    __esModule: true,
+    useResultCalculations: jest.fn(),
+  }),
+);
+
+jest.mock(
+  "@/hooks/redux/useResetFields.ts",
+  (): {
+    __esModule: boolean;
+    useResetFields: jest.Mock;
+  } => ({
+    __esModule: true,
+    useResetFields: jest.fn(),
+  }),
+);
+
+describe("ResultContainer", (): void => {
+  const setup = (): { container: HTMLElement } => {
+    const { container } = render(<ResultContainer />);
+    return { container };
+  };
+
+  const tipTotal: string = "10.00";
+  const totalSum: string = "110.00";
+  const resetAllFieldsMock: jest.Mock = jest.fn();
+
+  beforeEach((): void => {
+    (useResultCalculations as jest.Mock).mockReturnValue({
+      tipTotal,
+      totalSum,
+    });
+    (useResetFields as jest.Mock).mockReturnValue(resetAllFieldsMock);
+  });
+
+  it("renders div resultContainer", (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null =
+      container.querySelector(".resultContainer");
 
     expect(element).toBeInTheDocument();
+  });
+
+  it("renders component PriceContainer for tipTotal", (): void => {
+    setup();
+
+    const elements: HTMLElement[] = screen.getAllByTestId(priceContainerTestId);
+
+    expect(elements).toHaveLength(2);
+    expect(PriceContainer).toHaveBeenNthCalledWith(
+      1,
+      { priceAmount: tipTotal, priceType: PRICE_SECTION_LABEL_TEXT_AMOUNT },
+      undefined,
+    );
+  });
+
+  it("renders div resultContainerSpace", (): void => {
+    const { container } = setup();
+
+    const element: HTMLElement | null = container.querySelector(
+      ".resultContainerSpace",
+    );
+
+    expect(element).toBeInTheDocument();
+  });
+
+  it("renders component PriceContainer for totalSum", (): void => {
+    setup();
+
+    const elements: HTMLElement[] = screen.getAllByTestId(priceContainerTestId);
+
+    expect(elements).toHaveLength(2);
+    expect(PriceContainer).toHaveBeenNthCalledWith(
+      2,
+      { priceAmount: totalSum, priceType: PRICE_SECTION_LABEL_TEXT_TOTAL },
+      undefined,
+    );
+  });
+
+  it("renders component Button to reset AllFields", (): void => {
+    setup();
+
+    const element: HTMLElement = screen.getByTestId(buttonTestId);
+
+    expect(element).toBeInTheDocument();
+    expect(Button).toHaveBeenCalledWith(
+      {
+        handleButtonClick: resetAllFieldsMock,
+        isDisabled: false,
+        text: BUTTON_TEXT_RESET,
+      },
+      undefined,
+    );
   });
 });
--- src/components/container/ResultContainer/ResultContainer.tsx
+++ src/components/container/ResultContainer/ResultContainer.tsx
@@ -1,5 +1,5 @@
 import "./ResultContainer.scss";
-import { ReactElement, useCallback, useEffect, useState } from "react";
+import { ReactElement } from "react";
 import PriceContainer from "@/components/container/PriceContainer/PriceContainer.tsx";
 import Button from "@/components/atoms/Button/Button.tsx";
 import {
@@ -7,47 +7,12 @@ import {
   PRICE_SECTION_LABEL_TEXT_AMOUNT,
   PRICE_SECTION_LABEL_TEXT_TOTAL,
 } from "@/globals/constants/constants.ts";
-import { AppDispatch, RootState } from "@/redux/store.ts";
-import { useDispatch, useSelector } from "react-redux";
-import { resetBillValue } from "@/redux/slices/billSlice.ts";
-import { resetTipValue } from "@/redux/slices/tipSlice.ts";
-import { resetPeopleValue } from "@/redux/slices/peopleSlice.ts";
-
-const selectTipValue = (state: RootState) => state.tip.tipValue;
-const selectBillValue = (state: RootState) => state.bill.value;
-const selectPeopleValue = (state: RootState) => state.people.value;
+import { useResultCalculations } from "@/hooks/useResultCalculations.ts";
+import { useResetFields } from "@/hooks/redux/useResetFields.ts";
 
 const ResultContainer = (): ReactElement => {
-  const dispatch: AppDispatch = useDispatch();
-  const [tipTotal, setTipTotal] = useState<string>("");
-  const [totalSum, setTotalSum] = useState<string>("");
-
-  const tipAmount: number = useSelector(selectTipValue);
-  const billAmount: number = useSelector(selectBillValue);
-  const peopleCount: number = useSelector(selectPeopleValue);
-
-  useEffect(() => {
-    if (billAmount && tipAmount && peopleCount > 0) {
-      const calculatedTip: number = billAmount * (tipAmount / 100);
-      const calculatedTipPerPerson: string = (
-        calculatedTip / peopleCount
-      ).toFixed(2);
-      setTipTotal(calculatedTipPerPerson);
-
-      const total: number = billAmount + calculatedTip;
-      const totalPerPerson: string = (total / peopleCount).toFixed(2);
-      setTotalSum(totalPerPerson);
-    } else {
-      setTipTotal("");
-      setTotalSum("");
-    }
-  }, [billAmount, tipAmount, peopleCount]);
-
-  const resetAllFields = useCallback((): void => {
-    dispatch(resetBillValue());
-    dispatch(resetTipValue());
-    dispatch(resetPeopleValue());
-  }, [dispatch]);
+  const { tipTotal, totalSum } = useResultCalculations();
+  const resetAllFields = useResetFields();
 
   return (
     <div className="resultContainer">
