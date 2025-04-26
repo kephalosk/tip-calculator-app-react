1a6969f13220213488805b43dea013fc9abbdd0b
kephalosk <watchfromtheshore@gmail.com>
2025-04-26 18:32:29 +0200

    fic typo in waitForTimeout.spec.ts

--- src/globals/utils/waitForTimeout.spec.ts
+++ src/globals/utils/waitForTimeout.spec.ts
@@ -1,7 +1,7 @@
 import waitForTimeout from "./waitForTimeout";
 
 describe("waitForTimeout", (): void => {
-  it("should resolve after a delay", async (): Promise<void> => {
+  it("resolves after a delay", async (): Promise<void> => {
     const start: number = Date.now();
 
     await waitForTimeout();
