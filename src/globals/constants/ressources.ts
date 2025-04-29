1ab661bdf3e5c31578db0a6433d9622efbd521f1
kephalosk <watchfromtheshore@gmail.com>
2025-04-29 03:35:48 +0200

    test icon url part 2

--- src/globals/constants/ressources.ts
+++ src/globals/constants/ressources.ts
@@ -1,3 +1,3 @@
-export const PEOPLE_ICON_SRC: string = "/images/icon-person.svg";
-export const DOLLAR_ICON_SRC: string = "/images/icon-dollar.svg";
-export const LOGO_ICON_SRC: string = "/images/logo.svg";
+export const PEOPLE_ICON_SRC: string = "./images/icon-person.svg";
+export const DOLLAR_ICON_SRC: string = "./images/icon-dollar.svg";
+export const LOGO_ICON_SRC: string = "./images/logo.svg";
