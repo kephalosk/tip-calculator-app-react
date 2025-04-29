2ef5ed6cb84f0b352748b2b87fb7ee4ba9cbcad0
kephalosk <watchfromtheshore@gmail.com>
2025-04-29 03:34:15 +0200

    test icon url

--- src/globals/constants/ressources.ts
+++ src/globals/constants/ressources.ts
@@ -1,14 +1,3 @@
-import { ENVIRONMENT_DEVELOPMENT } from "@/globals/constants/constants.ts";
-
-export const PEOPLE_ICON_SRC: string =
-  import.meta.env.MODE === ENVIRONMENT_DEVELOPMENT
-    ? "./src/assets/images/icon-person.svg"
-    : "./images/icon-person.svg";
-export const DOLLAR_ICON_SRC: string =
-  import.meta.env.MODE === ENVIRONMENT_DEVELOPMENT
-    ? "./src/assets/images/icon-dollar.svg"
-    : "./images/icon-dollar.svg";
-export const LOGO_ICON_SRC: string =
-  import.meta.env.MODE === ENVIRONMENT_DEVELOPMENT
-    ? "./src/assets/images/logo.svg"
-    : "./images/logo.svg";
+export const PEOPLE_ICON_SRC: string = "/images/icon-person.svg";
+export const DOLLAR_ICON_SRC: string = "/images/icon-dollar.svg";
+export const LOGO_ICON_SRC: string = "/images/logo.svg";
